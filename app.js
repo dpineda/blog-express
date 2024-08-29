3659fae8b2ec88dfa5256bbb47ce8e3811a7f98f
Desi <desi@politicaldata.com>
2024-08-28 18:16:11 -0700

    display ip

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const os = require('os')
+
+const interfaces = os.networkInterfaces();
+const getNetworkAddress = () => {
+	for (const name of Object.keys(interfaces)) {
+		for (const interface of interfaces[name]) {
+			const {address, family, internal} = interface;
+			if (family === 'IPv4' && !internal) {
+				return address;
+			}
+		}
+	}
+};
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  const ip = getNetworkAddress()
+  console.log(`Example app listening on port ${port} ip: ${ip}`)
 })
