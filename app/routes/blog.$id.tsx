78c251a380721322dffb6d755703922bca010818
Desi <desi@politicaldata.com>
2024-11-13 17:51:01 -0800

    Make container smaller and show image in blog details

--- app/routes/blog.$id.tsx
+++ app/routes/blog.$id.tsx
@@ -19,12 +19,12 @@ export async function loader({params}: LoaderFunctionArgs) {
 export default function BlogEntry() {
   const data = useLoaderData<typeof loader>();
   const post = data[0];
-  
+
   return (
     <article>
       <h1 className="text-4xl font-bold mb-4">{post.title }</h1>
       <div className="flex items-center gap-3">
-        <img className="rounded-full" height="40" width="40" alt="" src={post.profile_image_url} />
+        <img className="rounded-full" height="70" width="70" alt="" src={post.profile_image_url} />
         <p className="text-gray-500">Published on {(new Date(post.created_at)).toLocaleDateString(
           'en-US', { 
             year: 'numeric', 
@@ -33,6 +33,13 @@ export default function BlogEntry() {
           })} by <a href="/" className="text-blue-600">{post.name}</a></p>
       </div>
       <hr className="my-8" />
+      {
+        post.featured_image_url && (<>
+          <img className="w-full" height="700" width="700" alt="" src={post.featured_image_url} />
+          <hr className="my-8" />
+        </>
+        )
+      }
       <div className="prose prose-lg max-w-none whitespace-pre-line">
         {post.content}
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,22 @@ export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    container: {
+      // you can configure the container to be centered
+      center: true,
+
+      // or have default horizontal padding
+      padding: '1rem',
+
+      // default breakpoints but with 40px removed
+      screens: {
+        sm: '400px',
+        md: '600px',
+        lg: '800px',
+        xl: '800px',
+        '2xl': '800px',
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
